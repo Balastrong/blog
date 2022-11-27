dc7f677e6d714c0b12e8edc32de60cbc75fae9f0
Leonardo Montini <lion.48m@gmail.com>
2022-11-27 10:33:19 +0100

    Improve 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,46 @@
 import { Layout, Seo } from "components";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import * as React from "react";
+import { SiteMetadata } from "types/gatsby";
 
-const NotFoundPage = ({ data, location }: any) => {
-  const siteTitle = data.site.siteMetadata.title;
+const NotFoundPage = ({
+  data: {
+    site: { siteMetadata },
+  },
+}: {
+  data: { site: { siteMetadata: SiteMetadata } };
+}) => {
+  const {
+    title,
+    social: { twitter, discord },
+  } = siteMetadata;
 
   return (
-    <Layout title={siteTitle}>
+    <Layout title={title}>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        Looks like the page you were looking for does not exist, how did that
+        happen? 🙃
+      </p>
+      <p>
+        If you think that page should exist, please let me know (on{" "}
+        <a href={twitter} target="_blank">
+          Twitter
+        </a>
+        ?{" "}
+        <a href={discord} target="_blank">
+          Discord
+        </a>
+        ? a{" "}
+        <a href="https://github.com/Balastrong/blog" target="_blank">
+          GitHub issue
+        </a>
+        ? Even a flying carrier pigeon would do) and I'll fix it.
+      </p>
+      <p>
+        In the meantime, you might want to get in safer places like the{" "}
+        <Link to="/">homepage</Link>.
+      </p>
     </Layout>
   );
 };
@@ -22,6 +54,10 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        social {
+          twitter
+          discord
+        }
       }
     }
   }
