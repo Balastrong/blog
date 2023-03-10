49fef1ba5475512ef54320c7a306db4bddf2e6a8
Leonardo Montini <lion.48m@gmail.com>
2023-03-10 16:29:46 +0100

    Env Server

--- src/pages/env.tsx
+++ src/pages/env.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
-const Env = () => {
-  const hello = process.env.GATSBY_HELLO_ENV;
+const Env = ({
+  serverData: { hello },
+}: {
+  serverData: {
+    hello: string;
+  };
+}) => {
   return (
     <div>
       <h1>Env</h1>
-      <p>{hello}</p>
+      <p>--{hello}--</p>
     </div>
   );
 };
 
 export default Env;
+
+export const getServerData = async () => {
+  const hello = process.env.GATSBY_HELLO_ENV;
+
+  return {
+    props: {
+      hello,
+    },
+  };
+};
