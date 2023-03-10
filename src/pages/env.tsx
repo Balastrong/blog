5fca54372c81fbeb47022d9a0a5f0f8fa71b8231
Leonardo Montini <lion.48m@gmail.com>
2023-03-10 18:07:41 +0100

    Aha

--- src/pages/env.tsx
+++ src/pages/env.tsx
@@ -1,7 +1,31 @@
+import { Seo } from "components";
+import { graphql } from "gatsby";
 import { getVideos } from "libs";
 import React from "react";
+import { Post, SiteMetadata } from "types/gatsby";
 
-const Env = ({ serverData }: any) => {
+const Env = ({
+  data: {
+    site: {
+      siteMetadata: {
+        author,
+        social: { youtube },
+      },
+    },
+    allMarkdownRemark: { nodes: posts },
+  },
+  serverData,
+}: {
+  data: {
+    site: {
+      siteMetadata: SiteMetadata;
+    };
+    allMarkdownRemark: {
+      nodes: Post[];
+    };
+  };
+  serverData: any;
+}) => {
   console.log("serverData", serverData);
 
   return (
@@ -13,11 +37,55 @@ const Env = ({ serverData }: any) => {
           <p>{video.snippet.title}</p>
         </div>
       ))}
+      {posts.map(post => (
+        <div key={post.id}>{post.frontmatter.title}</div>
+      ))}
     </div>
   );
 };
 
 export default Env;
+
+export const Head = () => <Seo title={"Hello"} />;
+
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        author {
+          name
+          summary
+        }
+        social {
+          youtube
+        }
+      }
+    }
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }, limit: 6) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          tags
+          featuredImage {
+            childImageSharp {
+              gatsbyImageData(
+                width: 500
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export const getServerData = async () => {
   const videos = await getVideos();
